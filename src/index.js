006bf206d3444bb72449bf903208d290e3ff7b9c
Felipe <felipebtu9@gmail.com>
2022-03-04 20:32:17 -0300

    Start styles and configs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Calculator</h1>
+      <Calculator />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
