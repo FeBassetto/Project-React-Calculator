4bbcea4fbba5f9bac44db4f0cbe922b83c988347
Felipe <felipebtu9@gmail.com>
2022-03-04 21:36:22 -0300

    Start grid

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Button.css'
+
+const Button = props => {
+
+
+
+    return(
+        <button className="button">{props.label}</button>
+    )
+}
+
+export default Button
