6b46777cd997861a6691d6d3715c3f3e79d0e4d9
Felipe <felipebtu9@gmail.com>
2022-03-05 01:13:48 -0300

    Display calculator

---
+++ src/components/Display.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Display.css'
+
+
+
+const Display = props => {
+
+
+
+    return(
+        <div className="display"> {props.value} </div>
+    )
+
+}
+
+
+export default Display
