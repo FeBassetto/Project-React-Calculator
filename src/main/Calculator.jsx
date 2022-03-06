24d5b2a6212685ed9846a4992857d5d49378e55a
Felipe <felipebtu9@gmail.com>
2022-03-06 00:46:16 -0300

    Finished calculator

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -32,7 +32,29 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if(this.state.current === 0){
+            this.setState({operation, current:1, clearDisplay:true})
+        }else{
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try{
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]} `)
+            } catch(err){
+                values[0] = this.state.values[0]
+            }
+           
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current:equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     addDigit(n) {
@@ -49,9 +71,10 @@ export default class Calculator extends Component {
         if(n !== '.'){
             const i = this.state.current
             const newValue = parseFloat(newDisplayValue)
-            const values = [...this.state.value]
+            const values = [...this.state.values]
             values[i] = newValue
             this.setState({values})
+            console.log(values)
         }
 
     }
